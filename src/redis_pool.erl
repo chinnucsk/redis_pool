c1cdb03efb99e4176849a3880b0ce4ec51049d3d
goofansu <goofan.su@gmail.com>
2013-05-01 10:09:14 +0800

    OTP style

--- src/redis_pool.erl
+++ src/redis_pool.erl
@@ -1,34 +1,5 @@
 -module(redis_pool).
--behavior(application).
--behavior(supervisor).
-
--export([start/0, stop/0]).
--export([start/2, stop/1]).
--export([init/1]).
-
 -export([q/2]).
-
-start() ->
-    application:start(?MODULE).
-
-stop() ->
-    application:stop(?MODULE).
-
-start(_Type, _Args) ->
-    supervisor:start_link({local, redis_pool}, ?MODULE, []).
-
-stop(_State) ->
-    ok.
-
-init([]) ->
-    {ok, Pools} = application:get_env(redis_pool, pools),
-    MapFun = fun({Name, SizeArgs, WorkerArgs}) ->
-                     PoolArgs = [{name, {local, Name}},
-                                 {worker_module, redis_worker}] ++ SizeArgs,
-                     poolboy:child_spec(Name, PoolArgs, WorkerArgs)
-             end,
-    PoolSpecs = lists:map(MapFun, Pools),
-    {ok, {{one_for_one, 10, 10}, PoolSpecs}}.
 
 q(PoolName, Command) ->
     Fun = fun(Worker) ->
