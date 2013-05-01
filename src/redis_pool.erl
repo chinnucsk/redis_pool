89c4af9d41a6db3810fcc9a2425756101badb245
goofansu <goofan.su@gmail.com>
2013-05-01 21:20:33 +0800

    Add start and stop.

--- src/redis_pool.erl
+++ src/redis_pool.erl
@@ -1,6 +1,14 @@
 -module(redis_pool).
+-export([start/0, stop/0]).
 -export([q/2]).
 
+start() ->
+    application:start(redis_pool).
+
+stop() ->
+    application:stop(redis_pool).
+
+%% eredis api
 q(PoolName, Command) ->
     Fun = fun(Worker) ->
                   gen_server:call(Worker, {q, Command})
